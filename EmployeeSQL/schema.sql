26a4fef41dfe89877a29076a3f13189110c3995b
Nathan Wong <nathanwong@Nathans-MacBook-Pro.local>
2019-09-24 23:27:51 -0500

    cleaned up schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,6 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
-
-CREATE TABLE "departments" (
+﻿CREATE TABLE "departments" (
     "dept_no" varchar(4) PRIMARY KEY,
     "dept_name" varchar(30)   NOT NULL,
 );
@@ -59,3 +57,48 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
+/*
+Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+
+departments
+---
+dept_no varchar(4) PK
+dept_name varchar(30)
+
+dept_emp
+---
+emp_no int PK FK - employees.emp_no
+dept_no varchar(4) FK >- departments.dept_no
+from_date varchar(10)
+to_date varchar(10)
+
+dept_manager
+---
+dept_no varchar(4) FK >- departments.dept_no
+emp_no int PK FK - employees.emp_no
+from_date varchar(10)
+to_date varchar(10)
+
+employees
+---
+emp_no int PK
+first_name varchar(30)
+last_name varchar(30)
+gender varchar(1)
+hire_date varchar(10)
+
+salaries
+---
+emp_no int PK FK - employees.emp_no
+salary int
+from_date varchar(10)
+to_date varchar(10)
+
+titles
+---
+emp_no int PK FK - employees.emp_no
+title varchar(60)
+from_date varchar(10)
+to_date varchar(10)
+
+*/
