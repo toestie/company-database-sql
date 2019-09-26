dd0555201bcf66d8adaefe95007da68c85bf51e4
Nathan Wong <nathanwong@Nathans-MacBook-Pro.local>
2019-09-25 19:40:01 -0500

    queries all work

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -6,7 +6,7 @@ INNER JOIN salaries as s
 ON e.emp_no = s.emp_no;
 
 -- 2. List employees who were hired in 1986.
-SELECT emp_no, first_name, last_name FROM employees WHERE hire_date LIKE "1986%";
+SELECT emp_no, first_name, last_name FROM employees WHERE hire_date LIKE '1986%';
 
 -- 3. List the manager of each department with the following information:
 -- department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
@@ -19,46 +19,52 @@ ON dm.emp_no = e.emp_no;
 
 -- 4. List the department of each employee with the following information:
 -- employee number, last name, first name, and department name.
-
--- ERROR ERROR ERROR
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees as e
 INNER JOIN dept_emp as de
 ON e.emp_no = de.emp_no
-SELECT dept_name
-FROM departments
-WHERE dept_no = 
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no;
 
 -- 5. List all employees whose first name is "Hercules" and last names begin with "B."
-SELECT * FROM employees WHERE first_name="Hercules" AND last_name LIKE "B%";
+SELECT * FROM employees WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 -- 6. List all employees in the Sales department, including their
 -- employee number, last name, first name, and department name.
-SELECT emp_no, last_name, first_name --, d.dept_no
-FROM employees -- as e
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
 WHERE emp_no IN (
     SELECT emp_no
     FROM dept_emp
     WHERE dept_no IN (
         SELECT dept_no
         FROM departments
-        WHERE dept_name = "Sales"
+        WHERE dept_name = 'Sales'
     )
 );
 
 -- 7. List all employees in the Sales and Development departments, including their
 -- employee number, last name, first name, and department name.
-SELECT emp_no, last_name, first_name --, d.dept_no
-FROM employees -- as e
-WHERE emp_no IN (
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE e.emp_no IN (
     SELECT emp_no
     FROM dept_emp
     WHERE dept_no IN (
         SELECT dept_no
         FROM departments
-        WHERE dept_name = "Sales" OR "Development"
+        WHERE dept_name = 'Sales' OR 'Development'
     )
 );
+
 -- 8. In descending order, list the frequency count of employee last names,
 -- i.e., how many employees share each last name.
 SELECT last_name, COUNT(last_name) as "Last Name Count" FROM employees GROUP BY last_name ORDER BY "Last Name Count" DESC;
